f1af43af3ca68ecc0fdf9f18acc25df677aa07ab
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-07-10 10:24:37 -0400

    Stub out project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PropertyListComponent } from './components/property-list/property-list.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PropertyDetailComponent } from './components/property-detail/property-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    //canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'properties',
+        component: PropertyListComponent,
+      },
+      {
+        path: 'properties/:id',
+        component: PropertyDetailComponent,
+      },
+    ],
+  },
+  { path: '**', component: NotFoundComponent },
+];
